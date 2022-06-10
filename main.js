ba486b3fd70d666c98deea5caa60404dabf7a18e
Johnson <johnsonkarasanyi@gmail.com>
2022-06-10 11:46:01 +0200

    Javascript OOP

--- main.js
+++ main.js
@@ -61,10 +61,10 @@ const arr2 = [...arr, 4];
 // // we can use combination of spread operator with Filter
 const arr3 = [...arr.filter(num => num !== 2),5];
 //console.log(arr3)
-const person1 = {
-  name: 'Johnson',
-  age: 36
-};
+// const person1 = {
+//   name: 'Johnson',
+//   age: 36
+// };
 
 // const person2 = {
 //   ...person1,
@@ -92,44 +92,45 @@ const person1 = {
 
 //=================================================
 // CLASSES
-// class Person {
-//   constructor(name, age) {
-//     this.name = name;
-//     this.age = age;
-//   }
-// };
-// const person1= new Person('Jonny',23);
-// const person2 = new Person('Sarah', 33);
-// console.log(person2)
-
 class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
-  };
-  greet() {
-    return `Hello, my name is ${this.name} and I am ${this.age}`;
-  };
-}
+  }
+};
+// const person1= new Person('Jonny',23);
+// const person2 = new Person('Sarah', 33);
+// console.log(person2, person1)
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   };
+//   greet() {
+//     return `Hello, my name is ${this.name} and I am ${this.age}`;
+//   };
+
+// }
 
 // const person1 = new Person('John', 33);
 // const person2 = new Person('Sara', 28);
-//console.log(person1.greet())
+// console.log(person1.greet())
 
 // //SUBCLASSES;
-class Customer extends Person {
-  constructor(name, age, balance) {
-    super(name, age);
-    this.balance = balance;
-  }
+// class Customer extends Person {
+//   constructor(name, age, balance) {
+//     super(name, age);
+//     this.balance = balance;
+//   }
 
-  info() {
-    return `${this.name} owes $${this.balance}.00`;
-  }
-}
+//   info() {
+//     return `${this.name} owes $${this.balance}.00`;
+//   }
+// }
 
-const customer1 = new Customer('Kevin', 32, 300);
-console.log(customer1.info())
+// const customer1 = new Customer('Kevin', 32, 300);
+// console.log(customer1.info())
 
 // MODULES
 
